ae262897e2539ef57f2fd44f8e30ddcfb5e1838e
BernatQuintilla <bernat.quintilla01@estudiant.upf.edu>
2024-11-28 16:36:32 +0100

    Search Update

--- search-engine-web-app/myapp/search/algorithms.py
+++ search-engine-web-app/myapp/search/algorithms.py
@@ -1,6 +1,41 @@
-# TODO : copiar codi del tf_idf (millor importar com a pickle)
-def search_in_corpus(query):
-    # 1. create create_tfidf_index
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 
-    # 2. apply ranking
-    return ""
+def search_in_corpus(query: str, corpus: dict):
+    """
+    Perform a search in the corpus using TF-IDF and cosine similarity.
+    :param query: Search query string.
+    :param corpus: Dictionary of Document objects.
+    :return: A list of ranked results (document IDs and scores).
+    """
+    # Extract document IDs and processed content from the corpus
+    doc_ids = list(corpus.keys())
+    documents = [' '.join(doc.processed) for doc in corpus.values()]  # Join the list of tokens into a string
+
+    # Add the query to the list for vectorization
+    all_text = documents + [query]
+
+    # Vectorize the corpus and the query using TF-IDF
+    vectorizer = TfidfVectorizer()
+    tfidf_matrix = vectorizer.fit_transform(all_text)  # TF-IDF matrix for documents + query
+
+    # Compute cosine similarity between query vector and document vectors
+    query_vector = tfidf_matrix[-1]  # The last vector corresponds to the query
+    doc_vectors = tfidf_matrix[:-1]  # All other vectors are documents
+    similarity_scores = cosine_similarity(query_vector, doc_vectors).flatten()
+
+    # Pair document IDs with their similarity scores and sort by score
+    ranked_results = sorted(
+        zip(doc_ids, similarity_scores),
+        key=lambda x: x[1],
+        reverse=True
+    )
+
+    # Return the ranked results
+    return ranked_results
+
+
+
+
+
+
--- search-engine-web-app/myapp/search/search_engine.py
+++ search-engine-web-app/myapp/search/search_engine.py
@@ -3,7 +3,7 @@ import random
 from myapp.search.objects import ResultItem, Document
 from myapp.search.algorithms import search_in_corpus
 
-# TODO : canviar per fer el ranking com a lab 2/3
+# We no longer need build_demo_results as we are using search_in_corpus
 def build_demo_results(corpus: dict, search_id):
     """
     Helper method, just to demo the app
@@ -17,28 +17,25 @@ def build_demo_results(corpus: dict, search_id):
         res.append(ResultItem(item.id, item.title, item.description, item.doc_date,
                               "doc_details?id={}&search_id={}&param2=2".format(item.id, search_id), random.random()))
 
-    # for index, item in enumerate(corpus['Id']):
-    #     # DF columns: 'Id' 'Tweet' 'Username' 'Date' 'Hashtags' 'Likes' 'Retweets' 'Url' 'Language'
-    #     res.append(DocumentInfo(item.Id, item.Tweet, item.Tweet, item.Date,
-    #                             "doc_details?id={}&search_id={}&param2=2".format(item.Id, search_id), random.random()))
-
-    # simulate sort by ranking
+    # Simulate sort by ranking (This is a placeholder, we'll remove it once we use actual search)
     res.sort(key=lambda doc: doc.ranking, reverse=True)
     return res
 
-# TODO : suposo que es poden filtrar els resultats diferent
 class SearchEngine:
-    """educational search engine"""
+    """Educational search engine"""
 
     def search(self, search_query, search_id, corpus):
         print("Search query:", search_query)
 
-        results = []
-        ##### your code here #####
-        results = build_demo_results(corpus, search_id)  # replace with call to search algorithm
+        # Now we are using the real search functionality, not the demo results
+        results = search_in_corpus(search_query, corpus)  # Call to search algorithm
 
-# TODO : la funció està a myapp/search/algorithms.py
-        # results = search_in_corpus(search_query)
-        ##### your code here #####
+        # We need to return the results as ResultItem objects, as per your initial setup
+        result_items = []
+        for doc_id, score in results:
+            doc = corpus[doc_id]  # Get the document from corpus using the doc_id
+            result_items.append(ResultItem(doc.id, doc.title, doc.description, doc.doc_date,
+                                           "doc_details?id={}&search_id={}&param2=2".format(doc.id, search_id), score))
 
-        return results
+        return result_items
+
